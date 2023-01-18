52fc27bf11c304eb0c962a8d4c92e53596582d77
Volodymyr Kurov <vokur13@gmail.com>
2023-01-18 12:38:24 +0200

    addContact resolved

---
+++ contacts.js
@@ -0,0 +1,66 @@
+const fs = require('fs').promises;
+const path = require('path');
+// const uid = require('uid');
+// import { uid } from 'uid';
+// uid();
+
+// const id = uid();
+// console.log(id);
+
+// import { nanoid } from 'nanoid'
+// model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
+
+// console.log(uid);
+
+const contactsPath = path.resolve('db/contactsDemo.json');
+
+async function listContacts() {
+  try {
+    const data = JSON.parse(await fs.readFile(contactsPath, 'utf8'));
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactsPath, 'utf8'));
+    const contactByID = await data.find((item) => item.id === contactId);
+    console.log(contactByID);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactsPath, 'utf8'));
+    const filteredList = await data.filter((item) => item.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(filteredList));
+    const newList = JSON.parse(await fs.readFile(contactsPath, 'utf8'));
+    console.log(newList);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactsPath, 'utf8'));
+    const newContact = { id: '12', name, email, phone };
+    const newList = [...data, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(newList));
+    const newData = JSON.parse(await fs.readFile(contactsPath, 'utf8'));
+    console.log(newData);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-const fs = require('fs').promises;
-const path = require('path');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require('./contacts');
+const { Command } = require('commander');
 
-// console.log(process.argv);
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
-const contactsPath = path.resolve('db/contacts.json');
+program.parse(process.argv);
 
-// fs.readFile(contactsPath, 'utf8')
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error.message));
+const argv = program.opts();
 
-// async function getData() {
-//   try {
-//     const data = await fs.readFile(contactsPath, 'utf8');
-//     console.log(data);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      listContacts();
+      break;
 
-// getData();
+    case 'get':
+      getContactById(id);
+      break;
 
-(async () => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf8');
-    console.log(data);
-  } catch (error) {
-    console.log(error.message);
+    case 'add':
+      addContact(name, email, phone);
+      break;
+
+    case 'remove':
+      removeContact(id);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
   }
-})();
+}
+
+invokeAction(argv);
